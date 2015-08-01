f6801c3a26573acf9526ae7f2681c36e722d8b07
TakamasaSugiura <takamasas2008@gmail.com>
2015-08-01 23:30:07 +0900

    TEST

---
+++ TwitterSnapshot/TwitterSnapshot/AssemblyInfo.fs
@@ -0,0 +1,20 @@
+﻿namespace TwitterSnapshot
+open System.Reflection
+open System.Runtime.CompilerServices
+
+[<assembly: AssemblyTitle("TwitterSnapshot")>]
+[<assembly: AssemblyDescription("")>]
+[<assembly: AssemblyConfiguration("")>]
+[<assembly: AssemblyCompany("")>]
+[<assembly: AssemblyProduct("")>]
+[<assembly: AssemblyCopyright("Takamasa")>]
+[<assembly: AssemblyTrademark("")>]
+
+// The assembly version has the format {Major}.{Minor}.{Build}.{Revision}
+
+[<assembly: AssemblyVersion("1.0.0.0")>]
+
+//[<assembly: AssemblyDelaySign(false)>]
+//[<assembly: AssemblyKeyFile("")>]
+
+()
---
+++ TwitterSnapshot/TwitterSnapshot/Camera.fs
@@ -0,0 +1,14 @@
+﻿namespace TwitterSnapshot
+
+open System
+open System.Collections.Generic
+open CoreTweet.CoreTweetFSharp
+open CoreTweet
+
+type Camera(consumerKey, consumerSecret, accessToken, accessSecret) = 
+    let m_token = Tokens.Create(consumerKey, consumerSecret, accessToken, accessSecret)
+
+    member this.TakeUser(screenName) =
+        let param = new Dictionary<string, Object>()
+        param.["screen_name"] <- screenName
+        m_token.Users.Show(param)
