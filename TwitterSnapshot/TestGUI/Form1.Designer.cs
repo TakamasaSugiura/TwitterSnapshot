740805a58fc1fd1464dfd32a8b80640e6da7daea
杉浦宜匡 <takamhxx_ybdc9_et@msn.com>
2015-08-11 00:24:35 +0900

    add a test project.

---
+++ TwitterSnapshot/TestGUI/Form1.Designer.cs
@@ -0,0 +1,88 @@
+﻿namespace TestGUI
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必要なデザイナー変数です。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 使用中のリソースをすべてクリーンアップします。
+        /// </summary>
+        /// <param name="disposing">マネージ リソースを破棄する場合は true を指定し、その他の場合は false を指定します。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows フォーム デザイナーで生成されたコード
+
+        /// <summary>
+        /// デザイナー サポートに必要なメソッドです。このメソッドの内容を
+        /// コード エディターで変更しないでください。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtScreenName = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.btnConnect = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // txtScreenName
+            // 
+            this.txtScreenName.Location = new System.Drawing.Point(12, 24);
+            this.txtScreenName.Name = "txtScreenName";
+            this.txtScreenName.Size = new System.Drawing.Size(179, 19);
+            this.txtScreenName.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(69, 12);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "ScreenName";
+            // 
+            // btnConnect
+            // 
+            this.btnConnect.Location = new System.Drawing.Point(197, 22);
+            this.btnConnect.Name = "btnConnect";
+            this.btnConnect.Size = new System.Drawing.Size(75, 23);
+            this.btnConnect.TabIndex = 2;
+            this.btnConnect.Text = "connect";
+            this.btnConnect.UseVisualStyleBackColor = true;
+            this.btnConnect.Click += new System.EventHandler(this.btnConnect_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 72);
+            this.Controls.Add(this.btnConnect);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtScreenName);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtScreenName;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btnConnect;
+    }
+}
+
--- TwitterSnapshot/TwitterSnapshot/Camera.fs
+++ TwitterSnapshot/TwitterSnapshot/Camera.fs
@@ -7,7 +7,7 @@ open System.Collections.Generic
 type Camera(consumerKey, consumerSecret, accessToken, accessSecret) = 
     let m_token = Tokens.Create(consumerKey, consumerSecret, accessToken, accessSecret)
     let m_records = new RecordModel()
-    member this.TakeUser(screenName) =
+    member this.TakeUser(screenName:string) =
         let param = new Dictionary<string, Object>()
         param.["screen_name"] <- screenName
         m_records.AddUser(m_token.Users.Show(param))
